cb51e2b9636848a9a54c6c67a3790e728aa1973b
DESKTOP-KK0QNHO\meren <merendmrcc@gmail.com>
2024-07-05 01:52:36 +0300

    veriler degistirildi

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('index', views.index),
     path('<kurs_adi>', views.detaylar),
     path('kategori/<int:category_id>', views.getCoursesByCategoryId),   
     path('kategori/<str:category_name>', views.getCoursesByCategoryName, name='courses_by_category_id'),
--- courses/views.py
+++ courses/views.py
@@ -1,39 +1,63 @@
+from datetime import date
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseNotFound
 from django.urls import reverse
 
-data = {
-    "programlama":"programlama kategorisine ait kurslar",
-    "web-gelistirme":"web gelistirme kategorisine ait kurslar",
-    "mobil":"mobil gelistirme kategorisine ait kurslar"
+db = {
+    "courses" : [
+        {
+            "title":"javascript kursu",
+            "description":"javascript kurs aciklamasi",
+            "imageUrl":"https://assets-btkakademi-gov-tr.akamaized.net/api/gallery/51/b4ecb192-1e71-428d-8f40-a4eb05862bdb/78.jpg?t=1641991152632",  
+            "slug":"javascript-kursu",
+            "date":date(2023,11,14),
+            "is_active":True,
+            "is_updated":False,
+        },
+        {
+            "title":"python kursu",
+            "description":"Python programlama dilini öğrenmek ve bu alanda yetkinlik kazanmak isteyenler için hazırlanan Python Programlama Kursu, hem başlangıç seviyesindeki hem de ileri düzeydeki katılımcılara yönelik kapsamlı bir eğitim sunmaktadır. Bu kurs, Python'un temellerinden başlayarak, ileri seviye konulara kadar geniş bir yelpazede bilgi ve beceri kazandırmayı amaçlamaktadır.",
+            "imageUrl":"https://assets-btkakademi-gov-tr.akamaized.net/api/gallery/51/799d49f3-287b-444a-adc8-e73afc4ceed8/76_0x220.jpg?t=1640260383870",  
+            "slug":"python-kursu",
+            "date":date(2023,5,12),
+            "is_active":True,
+            "is_updated":True,
+        },
+        {
+            "title":"mobil programlama kursu",
+            "description":"C# kurs aciklamasi",
+            "imageUrl":"https://assets-btkakademi-gov-tr.akamaized.net/api/gallery/51/10aff21b-0c0b-4bc4-b68c-dfda1ac33cb4/94.jpg?t=1640262259134",  
+            "slug":"mobil-programlama-kursu",
+            "date":date(2024,2,6),
+            "is_active":False,
+            "is_updated":False,
+        }
+    ],
+    "categories": [
+        {"id":1, "name":"programlama","slug":"programlama"},
+        {"id":2, "name":"web gelistirme","slug":"web gelistirme"},
+        {"id":3, "name":"mobil uygulamalar","slug":"mobil uygulamalar"},]
 }
 
 def index(req):
-    return render(req, template_name="courses/index.html")
+    courses = [course for course in db["courses"] if course["is_active"]]
+    categories = db["categories"]
 
-def kurslar(req):
-    category_list = list(data.keys())
-    list_items = ""
-    for category in category_list:
-        redirect_url = reverse('courses_by_category_id',args=[category])
-        list_items += f"""
-        <li>
-        <a href="{redirect_url}">
-        {category}
-        </a>
-        </li>"""
-
-        html_content = f"<h1>Kurs Kategorileri</h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(html_content)
+    return render(req, template_name="courses/index.html", context={
+        'categories': categories,
+        'courses':courses
+    })
 
 def detaylar(req, kurs_adi):
     return HttpResponse(f"{kurs_adi} DETAY SAYFASI gozukecek")
 
 def getCoursesByCategoryName(req, category_name):
     try:
-        category_text = data[category_name]
-        return HttpResponse(category_text)
+        category_text = db[category_name]
+        return render(req, template_name='courses/courses.html', context={
+            'category_name':category_name,
+            'category_text':category_text
+        })
     except:
         return HttpResponseNotFound("yanlis kategori secimi")
     
