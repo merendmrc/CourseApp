7be5cf6c3f4e93216d4ac1eb177258cd6a916f7b
DESKTOP-KK0QNHO\meren <merendmrcc@gmail.com>
2024-07-09 02:35:59 +0300

    .

--- courses/views.py
+++ courses/views.py
@@ -4,42 +4,6 @@ from django.http import HttpResponse,HttpResponseNotFound,Http404
 from django.urls import reverse
 from .models import Course,Category
 
-db = {
-    "courses" : [
-        {
-            "title":"javascript kursu",
-            "description":"javascript kurs aciklamasi",
-            "imageUrl":"js.jpg",  
-            "slug":"javascript-kursu",
-            "date":date(2023,11,14),
-            "is_active":True,
-            "is_updated":False,
-        },
-        {
-            "title":"python kursu",
-            "description":"Python programlama dilini öğrenmek ve bu alanda yetkinlik kazanmak isteyenler için hazırlanan Python Programlama Kursu, hem başlangıç seviyesindeki hem de ileri düzeydeki katılımcılara yönelik kapsamlı bir eğitim sunmaktadır. Bu kurs, Python'un temellerinden başlayarak, ileri seviye konulara kadar geniş bir yelpazede bilgi ve beceri kazandırmayı amaçlamaktadır.",
-            "imageUrl":"python.png",  
-            "slug":"python-kursu",
-            "date":date(2023,5,12),
-            "is_active":True,
-            "is_updated":True,
-        },
-        {
-            "title":"mobil programlama kursu",
-            "description":"mobil programlama kurs aciklamasi",
-            "imageUrl":"react.jpg",  
-            "slug":"mobil-programlama-kursu",
-            "date":date(2024,2,6),
-            "is_active":False,
-            "is_updated":False,
-        }
-    ],
-    "categories": [
-        {"id":1, "name":"programlama","slug":"programlama"},
-        {"id":2, "name":"web gelistirme","slug":"web-gelistirme"},
-        {"id":3, "name":"mobil uygulamalar","slug":"mobil-uygulamalar"},]
-}
-
 def index(req):
     courses = Course.objects.all()
     categories = Category.objects.all()
