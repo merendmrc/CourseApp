c0aa9fbdfd0f8ce1b3fe7eeae537bc3d5b3d6bd4
DESKTOP-KK0QNHO\meren <merendmrcc@gmail.com>
2024-07-12 03:57:07 +0300

    paginator eklendi

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,19 @@
 from datetime import date
 from django.shortcuts import get_object_or_404, render,redirect
-from django.http import HttpResponse,HttpResponseNotFound,Http404
-from django.urls import reverse
 from .models import Course,Category
+from django.core.paginator import Paginator
 
 def index(req):
     courses = Course.objects.all()
     categories = Category.objects.all()
 
+    paginator = Paginator(courses, 2)
+    page = req.GET.get('page',1)
+    page_obj = paginator.page(page)
+
     return render(req, template_name="courses/index.html", context={
         'categories': categories,
-        'courses':courses
+        'page_obj': page_obj
     })
 
 def details(req, slug):
@@ -22,10 +25,15 @@ def details(req, slug):
 
 
 def getCoursesByCategoryName(req, slug):
-    kurslar = Course.objects.filter(categories__slug=slug)
+    kurslar = Course.objects.filter(categories__slug=slug).order_by("is_active")
     kategoriler = Category.objects.all()
+
+    paginator = Paginator(kurslar, 2)
+    page = req.GET.get('page',1)
+    page_obj = paginator.page(page)
+
     return render(req, template_name="courses/index.html", context={
-        "courses" : kurslar,
+        "page_obj" : page_obj,
         "categories" : kategoriler,
         "selected_category_id" : Category.objects.get(slug=slug).id
     })
